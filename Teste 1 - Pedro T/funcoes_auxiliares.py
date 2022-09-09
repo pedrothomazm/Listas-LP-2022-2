81ae204da9332e612f9b2fc7f5e669177be85d9a
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2022-09-09 12:22:40 -0300

    Crie ndarray_is_in_fibonacci()

--- Teste 1 - Pedro T/funcoes_auxiliares.py
+++ Teste 1 - Pedro T/funcoes_auxiliares.py
@@ -66,3 +66,27 @@ def is_in_fibonacci(n):
         elem_a, elem_b = elem_b, elem_a + elem_b
     
     return n == elem_a
+
+
+def ndarray_is_in_fibonacci(array):
+    try:
+        # O parâmetro precisa ser um NumPy Array
+        if not isinstance(array, np.ndarray):
+            raise TypeError('O parâmetro deve ser um NumPy Array')
+        
+        # O parâmetro só pode conter inteiros
+        if not issubclass(array.dtype.type, np.integer):
+            raise TypeError('O tipo dos dados do Array deve ser inteiro')
+        
+        # O parâmetro não pode conter inteiros menores que 0 ou maiores que 1000
+        if np.any(array < 0) or np.any(array > 1000):
+            raise ValueError('O parâmetro deve conter apenas inteiros entre 0 e 1000')
+    
+    except Exception as error:
+        return error
+    
+    # Números de Fibonacci menores ou iguais a 1000
+    fib_0_to_1000 = fibonacci_less_than(1001)
+
+    # Verifica se cada elemento está na sequência
+    return np.isin(array, fib_0_to_1000)
